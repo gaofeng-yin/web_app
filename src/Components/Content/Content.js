c6280f0dc9f107791f0aef4d01df0582fd5a3b24
gaofeng <gaofengyin@hotmail.com>
2020-12-03 15:56:58 +0000

    inputing sections

--- src/Components/Content/Content.js
+++ src/Components/Content/Content.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Container from '@material-ui/core/Container';
+import Inputing from '../Inputing/Inputing';
 
 function Content(){
   return(
     <React.Fragment>
       <Container maxWidth="xl">
-
+        <Inputing />
       </Container>
     </React.Fragment>
   );
---
+++ src/Components/Inputing/Inputing.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import Grid from '@material-ui/core/Grid';
+
+
+function Inputing(){
+    return(
+        <div>
+
+        <div className="workout">
+            <Grid container direction="column" justify="space-around" alignItems="flex-start">
+                <h1>heee</h1>
+                <h2>dsdsa</h2>
+            </Grid>
+        </div>
+        
+        <div className="Outside_indoor">
+            <Grid container direction="column" justify="space-around" alignItems="flex-start">
+                <h1>Onde treinar?</h1>
+                <h1>....</h1>
+            </Grid>
+        </div>
+
+        <div className="Outside_indoor">
+            <Grid container direction="column" justify="space-around" alignItems="flex-start">
+                <h1>NUMERO DE EXERCICIOS</h1>
+                <h1>....</h1>
+            </Grid>
+        </div>
+
+        <div className="Outside_indoor">
+            <Grid container direction="column" justify="space-around" alignItems="flex-start">
+                <h1>Numero exercicios</h1>
+                <h1>....</h1>
+            </Grid>
+        </div>
+
+        <div className="Outside_indoor">
+            <Grid container direction="column" justify="space-around" alignItems="flex-start">
+                <h1>button</h1>
+                <h1>....</h1>
+            </Grid>
+        </div>
+
+        </div>
+    );
+}
+
+export default Inputing;
