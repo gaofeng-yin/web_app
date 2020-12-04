62870a6aec3e4eb5aeb2c265a3b7fd40ae284c2a
Andre Monteiro <monteiro.f27@hotmail.com>
2020-12-04 17:03:08 +0000

    Navbar quase

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,34 +1,34 @@
-import React from 'react'
+import React from 'react';
+import './Navbar.css';
+import logo from './Navbar_logo/Logo.png';
 
 const Navbar = () => {
     return (
-        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-  <a className="navbar-brand" href="#">Workout Planner</a>
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark Navbar_background">
+          <a className="navbar-brand" href="/">
+      <div className="logo-image">
+            <img src={logo} className="img-fluid" />
+      </div>
+</a>
+  
   <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span className="navbar-toggler-icon"></span>
   </button>
 
   <div className="collapse navbar-collapse" id="navbarSupportedContent">
+  <a className="navbar-brand" href="#">Workout Generator</a>
     <ul className="navbar-nav mr-auto">
       <li className="nav-item active">
         <a className="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
       </li>
       <li className="nav-item">
-        <a className="nav-link" href="#">link</a>
-      </li>
-      <li className="nav-item dropdown">
-        <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Dropdown
-        </a>
-        <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a className="dropdown-item" href="#">uma cena</a>
-          <a className="dropdown-item" href="#">outra cena</a>
-          <div className="dropdown-divider"></div>
-          <a className="dropdown-item" href="#">outra cena</a>
-        </div>
+        <a className="nav-link" href="#">About us</a>
       </li>
       <li className="nav-item">
-        <a className="nav-link" href="#">Link</a>
+        <a className="nav-link" href="#">Login</a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" href="#">Sign up</a>
       </li>
     </ul>
 
