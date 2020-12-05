a003250243bf605b7bd54e205c87dd1968dc5162
DiogoEPinto <56389291+DiogoEPinto@users.noreply.github.com>
2020-12-05 12:59:00 +0000

    dropdown (incomplete)

--- src/Components/Inputing/Inputing.js
+++ src/Components/Inputing/Inputing.js
@@ -6,84 +6,107 @@ import AccordionSummary from '@material-ui/core/AccordionSummary';
 import AccordionDetails from '@material-ui/core/AccordionDetails';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import { Col, Row } from 'react-bootstrap';
-import { Container } from '@material-ui/core';
+import { Container, makeStyles, MenuItem } from '@material-ui/core';
 import Checkbox from '@material-ui/core/Checkbox';
+import Select from '@material-ui/core/Select';
+import FormControl from '@material-ui/core/FormControl';
+import InputLabel from '@material-ui/core/InputLabel';
 
-function Inputing(){
-    return(
+
+function Inputing() {
+
+    const [place, setPlace] = React.useState(0);
+
+    const updatePlace = (event) => {
+        console.warn(event.target.value);
+        setPlace(event.target.value);
+    }
+
+    return (
         <div className="inputing">
-        
-        <form>
-        <div className="workout">
-                <Accordion>
-                    <AccordionSummary className="summary" expandIcon={<ExpandMoreIcon/>}>
-                        <span className="title">Workout</span>
-                    </AccordionSummary>
-                    <AccordionDetails>
-                        <Container>
-                            <Row>
-                                <Col>
-                                <Checkbox color="primary" name="" />
-                                <label>Chest</label>
-                                </Col>
-                                <Col>
-                                <Checkbox color="primary" name="" />
-                                <label>Back</label>
-                                </Col>
-                            </Row>
-                            <Row>
-                            <Col>
-                                <Checkbox color="primary" name="" />
-                                <label>Leg</label>
-                                </Col>
-                                <Col>
-                                <Checkbox color="primary" name="" />
-                                <label>Shoulder</label>
-                                </Col>
-                            </Row>
-                            <Row>
-                            <Col>
-                                <Checkbox color="primary" name="" />
-                                <label>Bicep</label>
-                                </Col>
-                                <Col>
-                                <Checkbox color="primary" name="" />
-                                <label>Tricep</label>
-                                </Col>
-                            </Row>
-                        </Container>
-                    </AccordionDetails>
-                </Accordion>
-        </div>
-        
-        <div className="Outside_indoor">
-            <Grid container direction="column" justify="space-around" alignItems="flex-start">
-                <h1>Onde treinar</h1>
-                <h1>....</h1>
-            </Grid>
-        </div>
 
-        <div className="Outside_indoor">
-            <Grid container direction="column" justify="space-around" alignItems="flex-start">
-                <h1>NUMERO DE EXERCICIOS</h1>
-                <h1>....</h1>
-            </Grid>
-        </div>
+            <form>
+                <div className="workout">
+                    <Accordion>
+                        <AccordionSummary className="summary" expandIcon={<ExpandMoreIcon />}>
+                            <span className="title">Workout</span>
+                        </AccordionSummary>
+                        <AccordionDetails>
+                            <Container>
+                                <Row>
+                                    <Col>
+                                        <Checkbox color="primary" name="" />
+                                        <label>Chest</label>
+                                    </Col>
+                                    <Col>
+                                        <Checkbox color="primary" name="" />
+                                        <label>Back</label>
+                                    </Col>
+                                </Row>
+                                <Row>
+                                    <Col>
+                                        <Checkbox color="primary" name="" />
+                                        <label>Leg</label>
+                                    </Col>
+                                    <Col>
+                                        <Checkbox color="primary" name="" />
+                                        <label>Shoulder</label>
+                                    </Col>
+                                </Row>
+                                <Row>
+                                    <Col>
+                                        <Checkbox color="primary" name="" />
+                                        <label>Bicep</label>
+                                    </Col>
+                                    <Col>
+                                        <Checkbox color="primary" name="" />
+                                        <label>Tricep</label>
+                                    </Col>
+                                </Row>
+                            </Container>
+                        </AccordionDetails>
+                    </Accordion>
+                </div>
 
-        <div className="Outside_indoor">
-            <Grid container direction="column" justify="space-around" alignItems="flex-start">
-                <h1>Numero exercicios</h1>
-                <h1>....</h1>
-            </Grid>
-        </div>
+                <div className="Outside_indoor">
+                    <Grid container direction="column" justify="space-around" alignItems="flex-start">
 
-        <div className="Outside_indoor">
-            <Grid container direction="column" justify="space-around" alignItems="flex-start">
-                <h1>button submmit</h1>
-                <h1>....</h1>
-            </Grid>
-        </div>
-        </form>
+                        <InputLabel id="place-filled-label">Onde treinar</InputLabel>
+
+                        <FormControl variant="filled" className="inputForm"> 
+                            <Select lavelId="place-filled-label" id="place-select-filled" value={place} onChange={updatePlace}>
+                                <MenuItem value={1}>Opção 1</MenuItem>
+                                <MenuItem value={2}>Opção 2</MenuItem>
+                                <MenuItem value={3}>Opção 3</MenuItem>
+                                <MenuItem value={4}>Opção 4</MenuItem>
+                                <MenuItem value={5}>Opção 5</MenuItem>
+                            </Select>
+                        </FormControl>
+
+                    </Grid>
+                </div>
+
+                <div className="Outside_indoor">
+                    <Grid container direction="column" justify="space-around" alignItems="flex-start">
+                        <h1>NUMERO DE EXERCICIOS</h1>
+                        <h1>....</h1>
+                    </Grid>
+                </div>
+
+                <div className="Outside_indoor">
+                    <Grid container direction="column" justify="space-around" alignItems="flex-start">
+                        <h1>Numero exercicios</h1>
+                        <h1>....</h1>
+                    </Grid>
+                </div>
+
+                <div className="Outside_indoor">
+                    <Grid container direction="column" justify="space-around" alignItems="flex-start">
+                        <h1>button submmit</h1>
+                        <h1>....</h1>
+                    </Grid>
+                </div>
+            </form>
         </div>
     );
 }
