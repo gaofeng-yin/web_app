af6ae93b1cc54151cc23dc19d7670e27a4eef74c
Diogo Pinto <diogopintoes@gmail.com>
2021-01-03 15:58:17 +0000

    Login unfinished

--- src/App.js
+++ src/App.js
@@ -1,13 +1,40 @@
-import React from "react";
+import React, {useState} from "react";
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle";
+
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import Login from './Components/Login/Login';
+import Dashboard from './Components/Dashboard/Dashboard';
+import Preferences from './Components/Preferences/Preferences';
 import Navbar from "./Components/Navbar/Navbar";
 
 function App() {
+
+  const [token, setToken] = useState();
+
+  if(!token) {
+    return <Login setToken={setToken} />
+  }
+
   return (
     <>
-     <Navbar/>
+      <div className="wrapper">
+        <h1>Application</h1>
+        <BrowserRouter>
+          <Switch>
+            <Route path="/dashboard">
+              <Dashboard />
+            </Route>
+            <Route path="/preferences">
+              <Preferences />
+            </Route>
+          </Switch>
+        </BrowserRouter>
+      </div>
+
+      <Navbar />
     </>
   );
 }
